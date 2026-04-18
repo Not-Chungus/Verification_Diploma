d5547c4045c5b6447907cacd559da5d300744185
Ibrahim Mohamed PC <Ibrahimmmhanafy@hotmail.com>
2026-04-18 20:40:15 +0200

    ongoing Q1

---
+++ VS_code/Session4/Assignment4/Q1_ALSU/ALSU_tb.sv
@@ -0,0 +1,207 @@
+import class_pkg::*;
+module ALSU_tb;
+
+
+reg clk, cin, rst, red_op_A, red_op_B, bypass_A, bypass_B, direction, serial_in;
+opcode_e opcode;
+reg signed [2:0] A, B;
+
+wire [15:0] leds;
+wire signed [5:0] out;
+
+
+reg signed [5:0] expected_out;
+reg [15:0] expected_leds;
+
+
+ALSU #(
+    .INPUT_PRIORITY(INPUT_PRIORITY),
+    .FULL_ADDER(FULL_ADDER)
+    ) uut (.*);
+ALSU_golden #(
+    .INPUT_PRIORITY(INPUT_PRIORITY),
+    .FULL_ADDER(FULL_ADDER)
+    ) golden (.out(expected_out),.leds(expected_leds),.*);
+
+
+integer correct_count = 0;
+integer error_count = 0;
+
+//Class and Objects
+RandomControl cntrl = new();
+//CovPort myport;  ?
+
+//clk========================================
+initial begin
+    clk = 0; 
+    forever begin
+        #5 clk = ~clk;
+        cntrl.clk = clk;
+    end
+end
+
+
+initial begin
+    
+    correct_count = 0; error_count = 0;
+    expected_out = 0; expected_leds = 0;
+    $display("===================================================TestBench Start===================================================");
+
+
+    //Test Series 1: Assert reset
+    assert_reset();
+
+    //Test Series 2: [First loop: constraint 8 is off]
+    cntrl.opcode_rand_array.constraint_mode(0);
+    repeat(1000) begin  //take care when repeating for sequential testbenches 
+        assert(cntrl.randomize());                // as we check and stimulate after 1 tick
+    //use obejct's values
+        A = cntrl.A;
+        B = cntrl.B;
+        opcode = opcode_e'(cntrl.opcode);
+
+        cin = cntrl.cin; rst = cntrl.rst ;
+        red_op_A = cntrl.red_op_A; red_op_B = cntrl.red_op_B; 
+        bypass_A = cntrl.bypass_A; bypass_B = cntrl.bypass_B;
+        direction= cntrl.direction; serial_in = cntrl.serial_in;
+    
+    //wait for two clock cycles
+        @(negedge clk);
+        @(negedge clk);
+
+        check_result();
+        #1;
+
+    end
+
+    //Test Series 3: [Second loop: constraint 8 only on, else disabled]
+    //no bypasses,reduction or resetting
+    cntrl.constraint_mode(0);
+    cntrl.opcode_rand_array.constraint_mode(1);
+    repeat(3000) begin  //take care when repeating for sequential testbenches 
+        assert(cntrl.randomize());                // as we check and stimulate after 1 tick
+    //use obejct's values
+        A = cntrl.A;
+        B = cntrl.B;
+        
+        cin = cntrl.cin; rst = 0;
+        red_op_A = 0; red_op_B = 0; 
+        bypass_A = 0; bypass_B = 0;
+        direction= cntrl.direction; serial_in = cntrl.serial_in;
+
+        foreach(cntrl.my_opcodes[i])begin //6 iterations * 1000 times
+            cntrl.opcode = cntrl.my_opcodes[i]; //all valid from const 8
+            opcode = opcode_e'(cntrl.opcode); //we didnt drive opcode directly to log bins
+            //wait for two clock cycles
+            @(negedge clk); //all 6 iterations share the same i/p
+            @(negedge clk);
+
+            check_result();
+            #1;
+        end
+    end
+/*
+    //Test Series 4: Directed tests for transition of opcode by ascending order
+    repeat(100) begin
+        assert(cntrl.randomize());
+        A = cntrl.A;
+        B = cntrl.B;
+        cin = cntrl.cin; rst = 0;
+        red_op_A = 0; red_op_B = 0; 
+        bypass_A = 0; bypass_B = 0;
+        direction= cntrl.direction; serial_in = cntrl.serial_in;
+
+        foreach(cntrl.my_opcodes[i])begin
+            cntrl.opcode = i;
+            opcode = opcode_e'(cntrl.opcode);
+            //wait for two clock cycles
+            @(negedge clk); //all 6 iterations in order share the same i/p
+            @(negedge clk);
+
+            check_result();
+            #1;
+
+        end
+    end
+*/
+
+    cntrl.constraint_mode(1);
+    //Test Series 5: Assert reset again
+    assert_reset();
+
+
+    $display("%t: At end of test error count is %0d and correct count = %0d", $time, error_count, correct_count);
+    $display("====================================================TestBench End====================================================");
+    $stop;
+
+end
+
+
+
+task check_result;
+
+    if((out  == expected_out) && (leds == expected_leds))
+    correct_count = correct_count + 1;
+    else
+    error_count = error_count + 1;
+   
+    if (out  !== expected_out) begin
+        $display("========================================================================");
+        $display("Error: opcode = %s | bypass_A = %b | bypass_B = %b | red_op_A = %b | red_op_B = %b | cin = %b" , opcode.name, bypass_A,bypass_B,red_op_A, red_op_B,cin);
+        $display("direction = %b | serial_in = %b" , direction,serial_in);
+        $display("A: %d | B: %d", A, B);
+        $display("Expected out = %d | Actual = %d", out, expected_out);
+    end
+
+    if (leds  !== expected_leds) begin
+        $display("========================================================================");
+        $display("Error: opcode = %s | bypass_A = %b | bypass_B = %b | red_op_A = %b | red_op_B = %b | cin = %b" , opcode.name, bypass_A,bypass_B,red_op_A, red_op_B,cin);
+        $display("direction = %b | serial_in = %b" , direction,serial_in);
+        $display("A: %d | B: %d", A, B);
+        $display("Expected leds = %d | Actual = %d", leds, expected_leds);
+    end
+
+endtask
+
+
+
+task assert_reset;
+    @(negedge clk);
+    rst = 1;
+    @(negedge clk);
+    @(negedge clk);
+    if (expected_out !== 0) begin
+        error_count = error_count + 1;
+        $display("Error Reseting");
+    end
+    else
+        correct_count = correct_count + 1;
+    @(negedge clk);
+    rst = 0;
+endtask
+
+
+//Sampling for Bins==========================
+always @(posedge clk)
+    cntrl.CovPort.sample();
+always @(posedge rst or posedge bypass_A or posedge bypass_B)
+    cntrl.CovPort.stop();
+always @(negedge rst or negedge bypass_A or negedge bypass_B) begin
+    if(!rst && !bypass_A && !bypass_B)
+        cntrl.CovPort.start();
+    else 
+        cntrl.CovPort.stop();
+end
+
+//or:
+/*
+always @(rst or bypass_A or bypass_B) begin
+    if (rst || bypass_A || bypass_B) 
+        myport.stop();
+    else 
+        myport.start();
+end
+*/
+
+
+endmodule
---
+++ VS_code/Session4/Assignment4/Q1_ALSU/package.sv
@@ -0,0 +1,194 @@
+package class_pkg;
+
+    parameter INPUT_PRIORITY = "A";
+    parameter FULL_ADDER = "ON";
+
+    typedef enum reg [2:0] {OR, XOR, ADD, MULT, SHIFT, ROTATE, INVALID_6, INVALID_7} opcode_e;
+    localparam MAXPOS = 3, ZERO = 0, MAXNEG = -4;
+
+
+    class RandomControl;
+        rand logic rst, red_op_A, red_op_B, bypass_A, bypass_B, cin, direction, serial_in;
+        rand logic [2:0] opcode;
+        rand logic signed [2:0] A, B;
+
+        bit clk;
+        rand opcode_e my_opcodes[6];
+
+    //========================================CONSTRAINTS========================================
+        constraint Reset { //off for 90% of the time
+            rst dist {0:/90 , 1:/10};
+        }
+
+        constraint Input { //MAXs and Zero 80% of the time for ADD and MULT
+            if((opcode == ADD) | (opcode == MULT)){
+                A dist {MAXPOS:=40,
+                    MAXNEG:=40,
+                    ZERO:=40,
+                    [-3:-1], [1:2] := 15};
+                B dist {MAXPOS:=40,
+                    MAXNEG:=40,
+                    ZERO:=40,
+                    [-3:-1], [1:2] := 15}; //15/135
+            }
+        }
+        
+        constraint Bypass { //off for 90% of the time
+            bypass_A dist {0:/90 , 1:/10};
+            bypass_B dist {0:/90 , 1:/10};
+        }
+
+        constraint Valdity { //Invalid for 10% of the time
+            opcode dist {INVALID_6:/10,INVALID_7:/10,
+                        [OR:ROTATE]:/80};
+            if(opcode ==  (OR | XOR)){
+                red_op_A dist{0:/50 , 1:/50};  //both red high is unlikely?
+                if(red_op_A){
+                    red_op_B dist{0:/80 , 1:/20};
+                }
+            }
+        }
+
+        constraint Reduction_Operations {       //selected has one bit set (
+            if(opcode inside {OR, XOR}){  //unslected has all low
+                if(red_op_A){
+                    A dist { 4:=80, 1:=80, 2:=80, [-3:0]:=20, 3:=20 };
+                    B == 3'b000;
+                }
+                else if(red_op_B){
+                    B dist { 4:=80, 1:=80, 2:=80, [-3:0]:=20, 3:=20 };
+                    A == 3'b000;
+                }
+            }                  
+        }
+
+        constraint opcode_rand_array {//"constraint 8"
+            unique {my_opcodes};
+            foreach(my_opcodes[i]){
+                my_opcodes[i] inside {[OR:ROTATE]};
+            }
+
+        }
+
+    //========================================COVER GROUPS========================================
+    covergroup CovPort; //@(posedge clk)
+        cin_cp: coverpoint cin iff(0)
+        direction_cp: coverpoint direction iff(0)
+        serial_in_cp: coverpoint serial_in iff(0)
+        red_op_A_cp: coverpoint red_op_A iff(0)
+        red_op_B_cp: coverpoint red_op_B iff(0)
+        
+
+
+        A_cp: coverpoint A iff(1)
+        {
+            bins A_data_0 = {ZERO};
+            bins A_data_max = {MAXPOS};
+            bins A_data_min = {MAXNEG};
+            bins A_data_default = default;
+            bins A_data_walkingones[] = {3'b001,3'b010,3'b100} iff(red_op_A);
+        }
+
+        B_cp: coverpoint B iff(1)
+        {
+            bins B_data_0 = {ZERO};
+            bins B_data_max = {MAXPOS};
+            bins B_data_min = {MAXNEG};
+            bins B_data_default = default;
+            bins B_data_walkingones[] = {3'b001,3'b010,3'b100} iff(!red_op_A && red_op_B);
+        }
+
+        ALU_cp: coverpoint opcode iff(1) 
+        {
+            bins Bins_shift[] = {SHIFT,ROTATE};
+            bins Bins_arith[] = {ADD, MULT};
+            bins Bins_bitwise[] = {OR, XOR};
+            illegal_bins Bins_invalid = {INVALID_6, INVALID_7};
+            bins Bins_trans = (0 => 0 => 1 => 1 => 2 => 2 => 3 => 3 => 4 => 4 => 5 => 5);
+            //the transition sequence is like this as we wait two cycles before changing
+            //opcode in the testbench
+        }
+        //================Cross Coverage=====================
+        
+        //1.
+        cross ALU_cp,A_cp{
+            
+            bins Arith_corners_A_1 = binsof(ALU_cp.Bins_arith) && 
+                                    binsof(A_cp.ZERO);
+            bins Arith_corners_A_2 = binsof(ALU_cp.Bins_arith) && 
+                                    binsof(A_cp.MAXPOS);
+            bins Arith_corners_A_3 = binsof(ALU_cp.Bins_arith) && 
+                                    binsof(A_cp.MAXNEG);
+            option.cross_auto_bin_max = 0;
+        }
+
+        cross ALU_cp,B_cp{   
+            bins Arith_corners_B_1 = binsof(ALU_cp.Bins_arith) && 
+                                    binsof(B_cp.ZERO);
+            bins Arith_corners_B_2 = binsof(ALU_cp.Bins_arith) && 
+                                    binsof(B_cp.MAXPOS);
+            bins Arith_corners_B_3 = binsof(ALU_cp.Bins_arith) && 
+                                    binsof(B_cp.MAXNEG);
+            option.cross_auto_bin_max = 0;
+        }
+
+        //2.
+        cross ALU_cp,cin_cp{
+            bins Add_cin = binsof(ALU_cp.Bins_arith[0]) && 
+                                    binsof(cin_cp);
+            option.cross_auto_bin_max = 0;
+        }
+
+        //3.
+        cross ALU_cp,direction_cp{
+            bins Shifts_direction = binsof(ALU_cp.Bins_shift) && 
+                                    binsof(direction_cp);
+            option.cross_auto_bin_max = 0;
+        }
+        //4.
+        cross ALU_cp,serial_in_cp{
+            bins Shift_Sin = binsof(ALU_cp.Bins_shift[0]) && 
+                                    binsof(serial_in_cp);
+            option.cross_auto_bin_max = 0;
+        }
+        //5.
+        cross ALU_cp,A_cp,red_op_A_cp
+        {
+            bins bitwise_red_A = binsof(ALU_cp.Bins_bitwise) && 
+                                    binsof(serial_in_cp) && binsof(red_op_A_cp) intersect{1};
+            option.cross_auto_bin_max = 0;
+        }
+        //6.
+        cross ALU_cp,B_cp,red_op_B_cp
+        {
+            bins bitwise_red_B = binsof(ALU_cp.Bins_bitwise) && 
+                                    binsof(serial_in_cp) && binsof(red_op_B_cp) intersect{1};
+            option.cross_auto_bin_max = 0;
+        }
+        //7.
+        cross ALU_cp,red_op_A_cp,red_op_B_cp
+        {
+            ignore bins red_at_no_bitwise = binsof(ALU_cp.Bins_bitwise) && 
+                                            binsof(red_op_A_cp) intersect{0} && 
+                                            binsof(red_op_B_cp) intersect{0};;
+            //option.cross_auto_bin_max = 0;
+        }
+
+
+
+    endgroup
+
+
+
+
+
+
+    function new();
+        CovPort = new;
+    endfunction
+
+
+    endclass
+
+
+endpackage
